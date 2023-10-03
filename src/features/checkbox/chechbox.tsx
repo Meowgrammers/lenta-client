eaef338258f69ab824617f0fcbd8c1eed1903b87
alheym <alheym.mw@gmail.com>
2023-10-03 10:46:23 +0300

    chechbox

---
+++ src/features/checkbox/chechbox.tsx
@@ -0,0 +1,41 @@
+import { FC, useEffect, useRef } from 'react'
+
+import { status } from '@/shared'
+
+interface CheckboxProps {
+  indeterminate: boolean
+  name: string
+  checked: boolean
+  id: string
+  compute: (checkboxId: string, status: number) => void
+}
+
+export const Checkbox: FC<CheckboxProps> = ({
+  indeterminate,
+  checked,
+  id,
+  compute,
+}) => {
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.indeterminate = indeterminate
+      inputRef.current.checked = checked
+    }
+  }, [indeterminate, checked])
+
+  return (
+    <input
+      type="checkbox"
+      ref={inputRef}
+      onChange={() => {
+        const newStatus = inputRef.current?.checked
+          ? status.checked
+          : status.unchecked
+        compute(id, newStatus)
+      }}
+      className="h-4 w-4"
+    />
+  )
+}
