22bcc5f362c8cca4070985a7be469ff9b7001169
alheym <alheym.mw@gmail.com>
2023-10-04 10:54:18 +0300

    icons

--- src/shared/ui/icons/product.tsx
+++ src/shared/ui/icons/product.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react'
 
-export const ProductIcon: FC = () => {
+import { IconType } from './type'
+
+export const ProductIcon: FC<IconType> = ({ className }) => {
   return (
     <svg
       width="24"
@@ -8,6 +10,7 @@ export const ProductIcon: FC = () => {
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      className={className}
     >
       <g id="list_alt">
         <path
