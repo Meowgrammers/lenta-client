1f43f7e81d6f38718e12b1ad20a9ebce25ee6a2a
alheym <alheym.mw@gmail.com>
2023-10-09 11:36:58 +0300

    readme

--- src/widgets/profileBlock/profileBlock.tsx
+++ src/widgets/profileBlock/profileBlock.tsx
@@ -5,7 +5,9 @@ export const ProfileBlock: FC = () => {
   return (
     <div className=" mb-8 flex justify-between gap-2 rounded-2xl bg-white/[0.16] p-3">
       <div className="flex h-10 w-10 items-center justify-center rounded-full border-2 border-white bg-[#c4c4c4]">
-        <span className="text-sm/[18px] font-semibold text-[#4D4D4D]">СП</span>
+        <span className="cursor-default text-sm/[18px] font-semibold text-[#4D4D4D]">
+          СП
+        </span>
       </div>
       <div className="flex flex-col gap-[2px] pr-3">
         <p className="text-profile-title font-semibold">
